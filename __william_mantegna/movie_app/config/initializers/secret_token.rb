d8e6744c844a56138f4c50508952f641ec779759
wmantegna <wmantegna@gmail.com>
2014-01-04 18:07:00 -0500

    added movie app

---
+++ __william_mantegna/movie_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieApp::Application.config.secret_key_base = '31449962ed98c93317021a3a9a3f7ada764468fb30254e61edfab546bad89e00b69ffa17badcab1041f7cfeae7004a05948020bf74a41702b0cf2fed63e190f8'
