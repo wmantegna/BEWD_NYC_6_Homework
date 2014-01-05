a21c225773f90019b29770129214bd114ab655a3
wmantegna <wmantegna@gmail.com>
2014-01-05 10:21:10 -0500

    Added CRUD to movie_app

--- __william_mantegna/movie_app/app/controllers/movies_controller.rb
+++ __william_mantegna/movie_app/app/controllers/movies_controller.rb
@@ -2,6 +2,39 @@ class MoviesController < ApplicationController
 	def index
 		@movies = Movie.all
 	end
+	
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		@movie = Movie.create(movie_params)
+		redirect_to "/movies/#{@movie.id}"
+	end
+
 	def show
+		@movie = Movie.find(params[:id])
+	end
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+
+		@movie.update_attributes(movie_params)
+		redirect_to movie_path
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		redirect_to movies_path
+	end
+
+	private
+	def movie_params
+		params.require(:movie).permit(:title, :description, :year_released)
 	end
 end
